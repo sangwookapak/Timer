d02d8e2b1de4ce8ed4b4492b770811874f05b45b
dit05 <dit@dit05ui-imac.local>
2019-03-28 00:11:49 -0700

    Initial Commit

---
+++ Alien ImageViwe Timer 02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alien ImageViwe Timer 02
+//
+//  Created by dit05 on 2019. 3. 28..
+//  Copyright © 2019년 dit05. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
